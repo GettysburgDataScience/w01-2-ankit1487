a926e1ab888ed34f1335a259a47b4cb8518b9da7
ankit1487 <118205239+ankit1487@users.noreply.github.com>
2024-08-29 12:48:06 -0400

    class exercise

---
+++ class exercise.py
@@ -0,0 +1,13 @@
+def calcArea(length, breadth):
+    Area = length * breadth
+    print(Area)
+    return
+
+def calcPerim(l, b):
+    Perimeteter = 2*(l+b)
+    print(Perimeteter)
+    return
+
+
+calcArea(5,10) #answer 50
+calcPerim(5,2) #answer 14
